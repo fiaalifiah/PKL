cb2b6d5c6c87381fb08f507bd0dc7138f3719f94
dhanyrevaldi <48001776+dhanyrevaldi@users.noreply.github.com>
2019-09-24 14:50:25 +0700

    kangen lala

--- app/src/main/java/com/apps/pkl/TerkiniFragment.java
+++ app/src/main/java/com/apps/pkl/TerkiniFragment.java
@@ -1,12 +1,6 @@
 package com.apps.pkl;
 
-
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,19 +8,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 /**
@@ -35,7 +21,7 @@ import static androidx.constraintlayout.widget.Constraints.TAG;
 public class TerkiniFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    SlidingUpPanelLayout layout;
+//    SlidingUpPanelLayout layout;
 
     public TerkiniFragment() {
     }
@@ -47,37 +33,6 @@ public class TerkiniFragment extends Fragment implements OnMapReadyCallback {
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        final ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
-        layout= (SlidingUpPanelLayout) rootView.findViewById(R.id.sliding_layout);
-        layout.setAnchorPoint(0.3f);
-        layout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
-            @Override
-            public void onPanelSlide(View panel, float slideOffset) {
-                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
-                if (slideOffset < 0.2) {
-                    if (actionBar.isShowing()) {
-                        actionBar.hide();
-                    }
-                } else {
-                    if (!actionBar.isShowing()) {
-                        actionBar.show();
-                    }
-                }
-            }
-
-            @Override
-            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
-
-            }
-        });
-//    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
-//        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
-//        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
-//        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-//        Canvas canvas = new Canvas(bitmap);
-//        vectorDrawable.draw(canvas);
-//        return BitmapDescriptorFactory.fromBitmap(bitmap);
     return rootView;
     }
     @Override
@@ -85,9 +40,9 @@ public class TerkiniFragment extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng jogja = new LatLng(-7.872954, 110.1440916);
+        mMap.addMarker(new MarkerOptions().position(jogja));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(jogja, 12.0f));
     }
-}
 
+}
