09c8d4ed5781ec817be1fa2239643f35a9715cd9
fiaalifiah <Alifiahwahyu123@gmail.com>
2019-09-24 15:36:59 +0700

    eeeh tole

---
+++ app/src/main/java/com/apps/pkl/DataHistoryFragment.java
@@ -0,0 +1,14 @@
+package com.apps.pkl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataHistoryFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_history_fragment);
+    }
+}
