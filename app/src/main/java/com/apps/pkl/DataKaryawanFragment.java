4c31a11ea43d7e898dcc399674bdabed5c295038
ninisseptia <48059826+ninisseptia@users.noreply.github.com>
2019-10-01 15:17:25 +0700

    Data Karyawan onClick

--- app/src/main/java/com/apps/pkl/DataKaryawanFragment.java
+++ app/src/main/java/com/apps/pkl/DataKaryawanFragment.java
@@ -1,13 +1,19 @@
 package com.apps.pkl;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 /**
@@ -17,15 +23,20 @@ public class DataKaryawanFragment extends Fragment {
 
 
     public DataKaryawanFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_datakaryawan, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_datakaryawan, container, false);
+        return rootView;
     }
-
+    public void pindahactivity(){
+        loadfrag(new DetailKaryawan() );
+    }
+    public void loadfrag(Fragment fr){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.containerView, new DetailKaryawan()).commit();
+    }
 }
